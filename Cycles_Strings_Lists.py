b0cedb190e0cb294277eadb2c25f5801720228b3
LidiyaChernova <32143269+LidiyaChernova@users.noreply.github.com>
2017-11-12 19:15:23 +0300

    Финальная версия кода

--- Cycles_Strings_Lists.py
+++ Cycles_Strings_Lists.py
@@ -3,16 +3,17 @@
 #Дано: aaaabbcccd
 #Надо: a4b2c3d1
 
-d = "aaabbcccf"
-j = 1
-
-for i in range (1, len(d)):
-    if d[i] == d[i - 1]:
-        j+=1
-    else:
-        print(d[i - 1], end = "")
-        print(j, end = ""),
-        j = 1
-
-print(d[i], end = ""),
-print(j)
+d = input()
+count = 1
+if len(d) == 1:
+    print(d[0], end="1")
+else:
+    for i in range (1, len(d)):
+        if d[i] == d[i - 1]:
+            count += 1
+        else:
+            print(d[i - 1], end = "")
+            print(count, end = ""),
+            count = 1
+    print(d[i], end = ""),
+    print(count)
