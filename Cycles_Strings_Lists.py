1a838e0c858083f7370efdbd2de5f3006eacb052
LidiyaChernova <32143269+LidiyaChernova@users.noreply.github.com>
2018-01-12 21:28:06 +0300

    удалила лишние пробелы в коде

--- Cycles_Strings_Lists.py
+++ Cycles_Strings_Lists.py
@@ -18,28 +18,17 @@ else:
     print(d[i], end = ""),
     print(count)
     
-    #Задание 2 - passed
-
+#Задание 2 - passed
 a = [int(i) for i in input().split()]
-
 a2 = [0]*len(a)
-
 if len(a) != 1:
-
     for i in range(len(a)-1):
-
         if i <= len(a):
-
             a2[i] = a[i+1] + a[i-1]
-
             print(a2[i], end=" ")
-
     a2[-1] += a[0] + a[-2]
-
     print(a2[-1])
-
 else:
-
     print(a[0])
     
 # Задание 3 - passed
